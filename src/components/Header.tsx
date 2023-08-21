70298ea34c9783f8e404a12c75e2c89d7c744dca
Nick Lewis <nick@TechEd-0006.techeducators.co.uk>
2023-08-21 16:01:40 +0100

    navbar responsive bits

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,53 +3,45 @@ import Link from "next/link";
 export default function Header() {
   return (
     <header className="bg-gray-900 p-4 md:p-6 flex justify-between items-center md:mx-auto max-w-screen-xl">
-      <h1 className="text-5xl md:text-6xl font-serif text-white animate-bounce md:hover:text-green-500 transition-colors duration-300">
+      <h1 className="text-4xl md:text-5xl font-serif text-white md:hover:text-green-500 transition-colors duration-300">
         blog<span className="text-red-500">ZILLA</span>
       </h1>
-      <nav className="hidden md:flex space-x-4 items-center">
-        <ul className="flex space-x-6">
-          <Link
-            href="/"
-            className="hover:text-gray-300 transition-colors duration-300"
-          >
-            Home
-          </Link>
-          <Link
-            href="/blog"
-            className="hover:text-gray-300 transition-colors duration-300"
-          >
-            Blog
-          </Link>
-          <Link
-            href="/blog/categories"
-            className="hover:text-gray-300 transition-colors duration-300"
-          >
-            Categories
-          </Link>
-          <Link
-            href="/contact"
-            className="hover:text-gray-300 transition-colors duration-300"
-          >
-            Contact
-          </Link>
+      <nav className="flex space-x-4 md:space-x-6 items-center text-sm md:text-base">
+        <ul className="flex space-x-4">
+          <li>
+            <Link
+              href="/"
+              className="text-white hover:text-gray-300 transition-colors duration-300"
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/blog"
+              className="text-white hover:text-gray-300 transition-colors duration-300"
+            >
+              Blog
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/blog/categories"
+              className="text-white hover:text-gray-300 transition-colors duration-300"
+            >
+              Categories
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/contact"
+              className="text-white hover:text-gray-300 transition-colors duration-300"
+            >
+              Contact
+            </Link>
+          </li>
         </ul>
       </nav>
-      <button className="md:hidden bg-red-500 border-2 border-red-700 p-2 rounded-lg">
-        <svg
-          className="w-6 h-6 text-white"
-          fill="none"
-          stroke="currentColor"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M4 6h16M4 12h16M4 18h16"
-          ></path>
-        </svg>
-      </button>
     </header>
   );
 }
